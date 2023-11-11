613f4caa4b5262c0333cbc155a097e70b9b4ac9c
Zach Willis <zachmw27@gmail.com>
2023-11-11 11:49:20 -0500

    complete

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (number1, number2) => number1 / number2;
+divide(2000,1000);
+
+const square = number3 => number3 ** 2;
+square(100);
+
+const add = (number4, number5) => number4 + number5;
+add(10,10);
